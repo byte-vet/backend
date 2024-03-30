0dae113cdce5290f33ee7760c248d9fbb15eea6c
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2024-03-30 14:39:50 -0300

    cria model de animal

---
+++ src/models/animalModel.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const Especies = Object.freeze(
+    {
+        CACHORRO: 'Cachorro',
+        GATO: 'Gato',
+        PEIXE: 'Peixe',
+        PASSARO: 'Pássaro',
+        COELHO: 'Coelho',
+        ROEDOR: 'Roedor',
+        REPTIL: 'Réptil',
+        OUTRO: 'Outro'
+    }
+)
+
+const animalSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    especie: {
+        type: String,
+        enum: Object.values(Especies),
+        required: true
+    },
+    raca: {
+        type: String,
+        required: true
+    },
+    idade: {
+        type: Number,
+        required: true
+    },
+    peso: {
+        type: Number,
+        required: true
+    },
+});
+
+Object.assign(animalSchema.statics, { Especies }); // Adiciona o objeto Especies ao modelo
+
+module.exports = mongoose.model('animal', animalSchema); //
