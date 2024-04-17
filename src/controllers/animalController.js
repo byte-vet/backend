78889fdc2697abf7eb8c41fa97261a4f2f9543d9
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2024-04-17 15:32:31 -0300

    fix: ao criar um animal, salva no atributo pets do usuario

--- src/controllers/animalController.js
+++ src/controllers/animalController.js
@@ -3,12 +3,16 @@ import Usuario from '../models/userModel.js';
 
 const createAnimal = async (req, res) => {
     const { usuario, nome, especie, raca, idade, peso } = req.body;
+    const user = await Usuario.findById(usuario);
     try {
-        if (!await Usuario.findById(usuario)) {
+        if (!user) {
             res.status(404).json({ message: `Usuário com id ${usuario} não encontrado` });
             return;
         }
         const newAnimal = await Animal.create({ usuario, nome, especie, raca, idade, peso });
+        user.pets = [...user.pets, newAnimal._id]; // adiciona o id do animal no array de pets do usuário
+        await user.save();
+
         res.status(201).json(newAnimal);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -38,7 +42,7 @@ const deleteAnimal = async (req, res) => {
     try {
         // adicionar tratamento de erros
         await Animal.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: `Animal com id ${req.params.id} removido com sucesso!` })
+        res.status(200).json({ message: `Animal com id ${req.params.id} removido com sucesso!` });
 
     } catch (error) {
         res.status(500).json({ message: error.message })
