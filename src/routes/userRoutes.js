a8970fd21bc0afed396612a51019f98e26f6f96b
Karen Aciole <karen.alves@ccc.ufcg.edu.br>
2024-04-25 13:30:08 -0300

    adiciona funcionalidade de atualizar dados do usuario

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUser } from '../controllers/userController.js';
+import { getUser, updateUser } from '../controllers/userController.js';
 import { checkToken } from '../middlewares/authorization.js';
 import { createAnimal, 
     getAllAnimalsByUser, 
@@ -10,8 +10,12 @@ import { createAnimal,
 
 const router = express.Router();
 
-router.post('/:id/pets/', checkToken, createAnimal); //http://localhost:3000/users/{{id_usuario}}/pets/
+/* Rotas relacionadas ao dados do usuario */
 router.get('/:id', checkToken, getUser); // http://localhost:3000/users/{{id_usuario}}
+router.put('/:id/', checkToken, updateUser); // http://localhost:3000/users/{{id_usuario}}
+
+/* Rotas relacionadas ao pet do usuario */
+router.post('/:id/pets/', checkToken, createAnimal); //http://localhost:3000/users/{{id_usuario}}/pets/
 router.get('/:id/pets', checkToken, getAllAnimalsByUser); // http://localhost:3000/users/{{id_usuario}}/pets
 router.get('/:id/pets/:id_pet', checkToken, getAnimalByUser); // http://localhost:3000/users/{{id_usuario}}/pets/{{id_pet}}
 router.delete('/:id/pets/:id_pet', checkToken, deleteAnimalByUser); // http://localhost:3000/users/{{id_usuario}}/pets/{{id_pet}}
