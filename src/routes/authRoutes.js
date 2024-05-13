f5c233b798705f89a3f8cd287f5b526231e89834
karen <96853561+karenaciole@users.noreply.github.com>
2024-05-13 11:31:24 -0300

    remove autenticacao do reset password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/signup', registerUser); // http://localhost:3000/auth/signup
 router.post('/login', loginUser); // http://localhost:3000/auth/login
 router.post('/requestResetPassword', requestResetPassword); // http://localhost:3000/auth/requestResetPassword
-router.post('/resetPassword', checkToken, resetPassword); // http://localhost:3000/auth/resetPassword
+router.post('/resetPassword', resetPassword); // http://localhost:3000/auth/resetPassword
 router.post('/vet/signup', registerVet); // Rota de registro de veterinário
 router.post('/vet/login', loginVet); // Rota de login de veterinário
 
